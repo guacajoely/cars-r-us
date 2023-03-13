b4c79ba127778d49946e17517957dd6b52c072ff
guacajoely <guacajoely@gmail.com>
2023-03-13 11:02:10 -0400

    refactored interior html

--- scripts/interiors.js
+++ scripts/interiors.js
@@ -3,19 +3,15 @@ const arrayOfInteriors = getInteriors()
 
 
 export const interiors = () => {
-    let html = `<select id="dropdown-interiors">
-                    <option value="0">Select an Interior</option>`
+    return `<select id="dropdown-interiors">
+                    <option value="0">Select an Interior</option>
 
-    // Use .map() for converting objects to <li> elements
-    const listItemsArray = arrayOfInteriors.map(interior => {
-        return `<option value="${interior.id}">${interior.name}</option>`
-    })
-
-    // Join all of the strings in the array into a single string
-    html += listItemsArray.join("")
-    html += "</select>"
-
-    return html
+   ${
+        arrayOfInteriors.map(interior => {
+            return `<option value="${interior.id}">${interior.name}</option>`
+        }).join("")
+    }
+            </select>`
 }
 
 
